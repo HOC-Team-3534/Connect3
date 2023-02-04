00baf90461632ecf3fc4cb2270e2f96ba8df6934
Malachi <malachijb28@gmail.com>
2023-02-03 19:29:59 -0500

    change wheel diameter to custom 3.81in instead of 4in

--- src/main/java/frc/robot/SwerveHelper.java
+++ src/main/java/frc/robot/SwerveHelper.java
@@ -1,19 +1,31 @@
 package frc.robot;
 
+import edu.wpi.first.math.util.Units;
 import frc.robot.Constants.Drive;
 import frc.statebasedcontroller.subsystem.general.swervedrive.swervelib.SDSModuleConfiguration;
 import frc.statebasedcontroller.subsystem.general.swervedrive.swervelib.SwerveConstants;
 
 public class SwerveHelper {
     public static boolean loadSwerveConstants() {
+        var config = Constants.Drive.Known.SDS_MODULE_CONFIGURATION;
+        var newConfig = new SDSModuleConfiguration(Units.inchesToMeters(3.81),
+                                                   config.angleGearRatio,
+                                                   config.driveGearRatio,
+                                                   config.angleKP,
+                                                   config.angleKI,
+                                                   config.angleKD,
+                                                   config.angleKF,
+                                                   config.driveMotorInvert,
+                                                   config.angleMotorInvert,
+                                                   config.canCoderInvert);
         SwerveConstants.fillNecessaryConstantsForFalcon(Drive.Calculated.MAX_FWD_REV_SPEED_MPS_EST,
                                                         Drive.Calculated.MAX_ROTATE_SPEED_RAD_PER_SEC_EST,
                                                         2 * Math.PI,
                                                         Drive.Calculated.KINEMATICS,
-                                                        SDSModuleConfiguration.SDSMK4(SDSModuleConfiguration.driveGearRatios.SDSMK4_L2),
-                                                        0.1, 0.0, 1.0 / 0.455,
-                                                        1 / 0.012, 10.0, 10.0,
-                                                        1, 1, 0.25, 0.25);
+                                                        newConfig, 0.1, 0.0,
+                                                        1.0 / 0.455, 1 / 0.012,
+                                                        10.0, 10.0, 1, 1, 0.25,
+                                                        0.25);
         SwerveConstants.createSwerveConstants();
         return true;
     }
